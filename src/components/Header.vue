9e76573c2086ea08d36508d7226c4047e9a7a052
Riki420 <girottiriccardo1998@gmail.com>
2021-09-15 12:15:06 +0200

    add background-img in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,23 @@
 <template>
   <header>
-      
+
   </header>
 </template>
 
 <script>
 export default {
-
+    name: 'Header'
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+    header{
+        background-image: url('../assets/images/footer-3.jpg');
+        background-size: cover;
+        background-position: top;
+        background-repeat: no-repeat;
+        height: 500px;
+
+    }
 
 </style>
