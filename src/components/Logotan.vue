c1b560b56c56dcab348560d8713b54031b18fae6
Riki420 <girottiriccardo1998@gmail.com>
2021-09-16 11:40:29 +0200

    created logotan component

--- src/components/Logotan.vue
+++ src/components/Logotan.vue
@@ -1,15 +1,61 @@
 <template>
-  <div class="container">
-
+  <div class="container-fluid p-4 bgDawnPink">
+    <div class="row">
+      <div class="col align-items-center">
+        <div class="container w-75">
+          <div class="row justify-content-center align-items-center">
+            <div class="col">
+              <figure>
+                <img
+                  src="../assets/images/ebay-logotan-2x.png"
+                  alt="ebay-logo"
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+            <div class="col">
+              <figure>
+                <img
+                  src="../assets/images/audible-logotan-2x.png"
+                  alt="ebay-logo"
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+            <div class="col">
+              <figure>
+                <img
+                  src="../assets/images/bb-logotan-2x.png"
+                  alt="ebay-logo"
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+            <div class="col">
+              <figure>
+                <img
+                  src="../assets/images/kindlefire-logotan-2x.png"
+                  alt="ebay-logo"
+                  class="img-fluid"
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Logotan'
-}
+  name: "Logotan",
+};
 </script>
 
-<style>
-
+<style lang="scss">
+@import "../assets/scss/_vars.scss";
+.bgDawnPink {
+  background-color: $dawnPink;
+}
 </style>
