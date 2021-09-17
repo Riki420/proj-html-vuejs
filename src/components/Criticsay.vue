6ed2950e4ad0163d8a593d937145a1e630e73ed2
Riki420 <girottiriccardo1998@gmail.com>
2021-09-17 10:57:09 +0200

    add testimonial text in criticsay

--- src/components/Criticsay.vue
+++ src/components/Criticsay.vue
@@ -21,6 +21,7 @@
                   </div>
                 </div>
                 <div class="row">
+                  <!--TESTIMONIAL IMG-->
                   <div class="col-6">
                     <div class="bg-testimonial round-right filter-bright">
                       <div class="text-white">
@@ -39,6 +40,51 @@
                           vitae enim quis sem pharetra. Curabitur arcu erat,
                           accumsan id imperdiet et, porttitor at sem.
                         </p>
+                        <address>
+                          <figure class="d-inline">
+                            <img
+                              src="../assets/images/nytimes-logo-white-xsmall.png"
+                              alt="NYT-logo"
+                              class="img-fluid me-2"
+                            />
+                          </figure>
+                          Justine Kilpatrick,
+                          <span class="keppel">The New York Times</span>
+                        </address>
+                      </div>
+                    </div>
+                  </div>
+                  <!--TESTIMONIALS TEXT-->
+                  <div class="col-6">
+                    <div class="container-fluid">
+                      <div class="row flex-column">
+                        <div class="col">
+                          <!--FIRST TESTIMONIAL BOX-->
+                          <div class="testimonials-box text-center">
+                            <h4 class="dm-serif">A True Masterpiece, Bravo!</h4>
+                            <p>
+                              Nulla quis lorem ut libero malesuada feugiat.
+                              Vivamus magna justo, lacinia eget consectetur sed,
+                              convallis at tellus. Vivamus scipit tortor eget
+                              felis porttitor volupat. Nulla quis lorem ut
+                              libero malesuada feug at.
+                            </p>
+                          </div>
+                          <div class="box-pointer"></div>
+                          <address>
+                            <figure class="d-inline me-2">
+                              <img
+                                src="../assets/images/theguardian-xsmall.png"
+                                alt="guardian-logo"
+                                class="img-fluid"
+                              />
+                            </figure>
+                            <span class="osloGray">Gerald Hendley</span>, The
+                            Guardian
+                          </address>
+                        </div>
+                        <!--SECOND TESTIMONIAL BOX-->
+                        <div class="col"></div>
                       </div>
                     </div>
                   </div>
@@ -59,19 +105,38 @@ export default {
 </script>
 
 <style lang="scss">
+@import "../assets/scss/_vars.scss";
+
+//Filter Class
 .round-right {
   border-radius: 0 0 40px 0;
 }
 .filter-bright {
   filter: brightness(0.5);
-  p {
-    filter: brightness(1);
-  }
 }
+
+//Testimonials class
 .bg-testimonial {
   background-image: url("../assets/images/testimonial2-2x.jpg");
   background-position: center;
   background-size: cover;
+  background-image: linear-gradient(0, 0, 0, 0.5);
   padding: 2em;
+}
+.testimonials-box {
+  min-height: 200px;
+  background-color: $dawnPink;
+  padding: 40px;
+}
+.box-pointer {
+  height: 30px;
+  width: 30px;
+  background: $dawnPink;
+  margin: 0 auto;
+  transform: rotate(45deg);
+  border-radius: 0 0 3px 0;
+  margin-top: -22px;
+  position: relative;
+  left: calc(0.2vw - 100px);
 }
 </style>
