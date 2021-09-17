238d74dc26319bfbd530e90d594d9e196182d3f0
Riki420 <girottiriccardo1998@gmail.com>
2021-09-17 15:52:24 +0200

    added article

--- src/components/Articles.vue
+++ src/components/Articles.vue
@@ -9,7 +9,7 @@
                 <div class="row">
                   <!--ARTICLE HEAD-->
                   <div
-                    class="col d-flex justify-content-between align-items-center"
+                    class="col-12 d-flex justify-content-between align-items-center"
                   >
                     <div>
                       <h1 class="dm-serif">Recent Articles</h1>
@@ -20,6 +20,64 @@
                     </button>
                   </div>
                   <!--LARGE ARTICLE-->
+                  <div class="col-8">
+                    <div class="card p-4">
+                      <div class="card-body">
+                        <figure>
+                          <img
+                            src="../assets/images/20-Tips-scaled.jpg"
+                            alt=""
+                            class="img-fluid"
+                          />
+                        </figure>
+                        <h4>20 Creativity Tips</h4>
+                        <address>
+                          By admin | October 24th, 2019 | Categories: Event,
+                          Tips & Tricks
+                        </address>
+                        <p>
+                          Vestibulum ante ipsum primis in faucibus orci luctus
+                          et ultices posuerre cubilia Curao. Donec velit neque,
+                          auctor sit amet aliquam vel, ulamcorper sit amet.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <!--SIDE ARTICLE-->
+                  <div class="col-4 d-flex flex-column">
+                    <div>
+                      <!--FIRST CARD-->
+                      <div class="card">
+                        <div class="card-body">
+                          <figure>
+                            <img
+                              src="../assets/images/thumb-13-2x-320x202.jpg"
+                              alt="thumb-13"
+                              class="img-fluid"
+                            />
+                          </figure>
+                          <h5>What's On Your Booklist</h5>
+                          <address>October 24th, 2019 | 0 Comments</address>
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <!--SECOND CARD-->
+                      <div class="card">
+                        <div class="card-body">
+                          <figure>
+                            <img
+                              src="../assets/images/thumb-08-2x-320x202.jpg"
+                              alt="thumb-13"
+                              class="img-fluid"
+                            />
+                          </figure>
+                          <h5>Instanbul Travel Guide</h5>
+                          <address>October 24th, 2019 | 0 Comments</address>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
